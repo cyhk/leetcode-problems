f5a9f32c5d6edfcfacb3753f8e17d409709a610f
cyhk <cyhk116@gmail.com>
2019-08-17 20:41:57 -0700

    solved problems 1 and 150

---
+++ 1-two-sum.js
@@ -0,0 +1,39 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that 
+ * they add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ * 
+ * Example:
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+  let numObj = {};
+  
+  // make hash table
+  for (let num of nums) {
+    if (numObj[num] === undefined) {
+      numObj[num] = 1;
+    } else {
+      numObj[num] = numObj[num]+ 1;
+    }
+  }
+  
+  for (let i=0; i<nums.length; i++) {
+    if (numObj[target-nums[i]]) {
+        let j = nums.findIndex(num => num === target-nums[i]);
+        if (j === i) continue;
+      return [i, j];
+    }
+  }
+};
