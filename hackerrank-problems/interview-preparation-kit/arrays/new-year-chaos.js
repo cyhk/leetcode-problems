d591b76fa60cebdcd5766b20357f2c1090391e27
cyhk <cyhk116@gmail.com>
2019-09-12 20:51:25 -0700

    solved new year chaos from hackerrank

---
+++ hackerrank-problems/interview-preparation-kit/arrays/new-year-chaos.js
@@ -0,0 +1,13 @@
+function minimumBribes(q) {
+  let swaps = 0;
+  for (let i=q.length-1; i>=0; i--) {
+    if (q[i] - i - 1 > 2) {
+      console.log('Too chaotic');
+      return;
+    }
+    for (let j=Math.max(0, q[i]-2); j<i; j++) {
+      if (q[j] > q[i]) swaps++;
+    }
+  }
+  console.log(swaps);
+}
