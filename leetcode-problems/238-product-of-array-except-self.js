06ee6ef0bb130b51152d9f7de19ed948391f10d8
cyhk <cyhk116@gmail.com>
2019-10-11 22:42:55 -0700

    fixed comments on 238

--- leetcode-problems/238-product-of-array-except-self.js
+++ leetcode-problems/238-product-of-array-except-self.js
@@ -8,7 +8,8 @@
  * Note: Please solve it without division and in O(n).
  * 
  * Follow up:
- * Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+ * Could you solve it with constant space complexity? (The output array does
+ * not count as extra space for the purpose of space complexity analysis.)
  * 
  * @param {number[]} nums
  * @return {number[]}
